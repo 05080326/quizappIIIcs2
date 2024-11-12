394b411eb75e409ad6d5d7779ca294133ca4475b
Joni <jdvalenccia@diwamail.com>
2024-11-12 15:48:27 +0800

    valencia UI

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -4,27 +4,26 @@ class startpage extends StatelessWidget {
   const startpage({super.key});
   @override
   Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromARGB(255, 78, 13, 151),
-        Color.fromARGB(255, 107, 15, 168)
-      ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/quiz-logo.png', width: 200),
-            const Text("Start Quiz",
-                style: TextStyle(fontSize: 20, color: Colors.white)),
-            IconButton(
-              iconSize: 72,
-              icon: const Icon(Icons.arrow_right_alt),
-              color: Colors.white,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 200),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text("Learn Flutter the fun way!",
+              style: TextStyle(
+                  fontSize: 24, color: Color.fromARGB(255, 237, 223, 252))),
+          const SizedBox(
+            height: 80,
+          ),
+          OutlinedButton.icon(
               onPressed: () {},
-            ),
-          ],
-        ),
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text("Start Quiz")),
+        ],
       ),
     );
   }
